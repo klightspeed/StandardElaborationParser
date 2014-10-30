0e9c9e0357bc999b91d8729732e65dec8045b046
Ben Peddell <klightspeed@killerwolves.net>
2014-10-30 10:43:27 +1000

    Move Term encapsulation to XmlLasdDatabase

--- StandardElaborationParser/Program.cs
+++ StandardElaborationParser/Program.cs
@@ -6,8 +6,6 @@ using System.Xml;
 using System.Xml.Linq;
 using System.Xml.XPath;
 using System.IO;
-using Google.Apis.Drive;
-using Google.GData.Spreadsheets;
 using DocumentFormat.OpenXml;
 using DocumentFormat.OpenXml.Packaging;
 using DocumentFormat.OpenXml.Wordprocessing;
@@ -22,7 +20,30 @@ namespace StandardElaborationParser
         public int RowSpan;
         public int ColSpan;
         public XElement[] Paragraphs;
-        public string Text;
+        public string Text
+        {
+            get
+            {
+                List<string> lines = new List<string>();
+
+                foreach (XElement para in Paragraphs)
+                {
+                    if (para.Name.LocalName == "p")
+                    {
+                        lines.Add(para.Value);
+                    }
+                    else if (para.Name.LocalName == "ul")
+                    {
+                        foreach (XElement li in para.Elements().Where(el => el.Name.LocalName == "li"))
+                        {
+                            lines.Add(" • " + li.Value);
+                        }
+                    }
+                }
+
+                return String.Join("\n", lines);
+            }
+        }
 
         public override string ToString()
         {
@@ -57,9 +78,9 @@ namespace StandardElaborationParser
 
     class Program
     {
-        static XNamespace ns_lasd = "http://tempuri.org/XmlLasdDatabase.xsd";
+        private static XNamespace ns_lasd = "http://tempuri.org/XmlLasdDatabase.xsd";
 
-        static Dictionary<string, string> YearLevels = new Dictionary<string, string>
+        private static Dictionary<string, string> YearLevels = new Dictionary<string, string>
         {
             { "prep", "Prep" },
             { "yr1", "Year 1" },
@@ -74,7 +95,7 @@ namespace StandardElaborationParser
             { "yr10", "Year 10" }
         };
 
-        static Dictionary<string, string> Subjects = new Dictionary<string, string>
+        private static Dictionary<string, string> Subjects = new Dictionary<string, string>
         {
             { "eng", "English" },
             { "math", "Mathematics" },
@@ -83,7 +104,7 @@ namespace StandardElaborationParser
             { "sci", "Science" }
         };
 
-        static IEnumerable<XNode> ParagraphContent(Paragraph para, Dictionary<string, Style> styles)
+        private static IEnumerable<XNode> ParagraphContent(Paragraph para, Dictionary<string, Style> styles)
         {
             foreach (Run run in para.Elements<Run>())
             {
@@ -94,7 +115,7 @@ namespace StandardElaborationParser
             }
         }
         
-        static IEnumerable<XElement> CellContent(TableCell wordcell, Dictionary<string, Style> styles)
+        private static IEnumerable<XElement> CellContent(TableCell wordcell, Dictionary<string, Style> styles)
         {
             XElement list = null;
 
@@ -160,29 +181,7 @@ namespace StandardElaborationParser
             }
         }
 
-        static string CellText(XElement[] paragraphs)
-        {
-            List<string> lines = new List<string>();
-
-            foreach (XElement para in paragraphs)
-            {
-                if (para.Name.LocalName == "p")
-                {
-                    lines.Add(para.Value);
-                }
-                else if (para.Name.LocalName == "ul")
-                {
-                    foreach (XElement li in para.Elements().Where(el => el.Name.LocalName == "li"))
-                    {
-                        lines.Add(" • " + li.Value);
-                    }
-                }
-            }
-
-            return String.Join("\n", lines);
-        }
-
-        static WordTable GetTable(Table tbl, Dictionary<string, Style> styles)
+        private static WordTable GetTable(Table tbl, Dictionary<string, Style> styles)
         {
             WordTable table = new WordTable();
             TableRow[] tblrows = tbl.Elements<TableRow>().ToArray();
@@ -230,7 +229,6 @@ namespace StandardElaborationParser
                             RowSpan = 1,
                             ColSpan = colspan,
                             Paragraphs = paragraphs,
-                            Text = CellText(paragraphs)
                         };
 
                         columns[tcno] = cell;
@@ -245,99 +243,7 @@ namespace StandardElaborationParser
             return table;
         }
 
-        static XElement FindTerms(XElement element, Dictionary<string, string> terms)
-        {
-            XElement ret = new XElement(element.Name, element.Attributes());
-
-            foreach (XNode node in element.Nodes())
-            {
-                if (node is XElement)
-                {
-                    ret.Add(FindTerms((XElement)node, terms));
-                }
-                else if (node is XText)
-                {
-                    string text = ((XText)node).Value;
-                    string lowertext = text.ToLower();
-                    int startpos = 0;
-
-                    do
-                    {
-                        int matchpos = text.Length;
-                        int matchlen = 0;
-                        string matchname = null;
-
-                        foreach (KeyValuePair<string, string> term in terms)
-                        {
-                            int tmatchpos = lowertext.IndexOf(term.Key, startpos);
-
-                            if (tmatchpos >= startpos && 
-                                (tmatchpos < matchpos ||
-                                 (tmatchpos == matchpos && term.Key.Length > matchlen)))
-                            {
-                                matchpos = tmatchpos;
-                                matchlen = term.Key.Length;
-                                matchname = term.Value;
-                            }
-                        }
-
-                        if (matchlen != 0)
-                        {
-                            if (matchpos != startpos)
-                            {
-                                ret.Add(new XText(text.Substring(startpos, matchpos - startpos)));
-                            }
-
-                            ret.Add(new XElement(ns_lasd + "term",
-                                new XAttribute("name", matchname),
-                                new XText(text.Substring(matchpos, matchlen))
-                            ));
-                        }
-                        else
-                        {
-                            ret.Add(new XText(text.Substring(startpos, text.Length - startpos)));
-                        }
-
-                        startpos = matchpos + matchlen;
-                    }
-                    while (startpos < text.Length);
-                }
-                else
-                {
-                    ret.Add(node);
-                }
-            }
-
-            return ret;
-        }
-
-        static void FindTerms(AchievementRowGroup group, Dictionary<string, string> terms)
-        {
-            foreach (AchievementRowGroup grp in group.Groups)
-            {
-                FindTerms(grp, terms);
-            }
-
-            foreach (AchievementRow row in group.Rows)
-            {
-                foreach (FormattedText text in row.Descriptors)
-                {
-                    text.Elements = text.Elements.Select(e => FindTerms(e, terms)).ToArray();
-                }
-            }
-        }
-
-        static void FindTerms(KeyLearningArea kla)
-        {
-            Dictionary<string, string> terms = kla.Terms.SelectMany(t => t.Keywords.Select(k => new { keyword = k.ToLower(), name = t.Name })).ToDictionary(kn => kn.keyword, kn => kn.name);
-
-            foreach (AchievementRowGroup group in kla.Groups)
-            {
-                FindTerms(group, terms);
-            }
-        }
-
-        static KeyLearningArea ProcessKLA(string yearLevel, string yearLevelId, string subject, string subjectId, WordTable[] tables)
+        private static KeyLearningArea ProcessKLA(string yearLevel, string yearLevelId, string subject, string subjectId, WordTable[] tables)
         {
             KeyLearningArea kla = new KeyLearningArea
             {
@@ -428,21 +334,21 @@ namespace StandardElaborationParser
                 }
             }
 
-            FindTerms(kla);
+            kla.FindTerms();
 
             return kla;
         }
 
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
             foreach (KeyValuePair<string, string> grade_kvp in YearLevels)
             {
                 foreach (KeyValuePair<string, string> subject_kvp in Subjects)
                 {
-                    string filename = Path.Combine(Environment.CurrentDirectory, @"ac_" + subject_kvp.Key + "_" + grade_kvp.Key + "_se");
+                    string filename = Path.Combine(Environment.CurrentDirectory, @"ac_" + subject_kvp.Key + "_" + grade_kvp.Key + "_se.docx");
                     Console.WriteLine("Processing {0} {1} ({2})", grade_kvp.Value, subject_kvp.Value, filename);
 
-                    WordprocessingDocument doc = WordprocessingDocument.Open(filename + ".docx", false);
+                    WordprocessingDocument doc = WordprocessingDocument.Open(filename, false);
                     Body body = doc.MainDocumentPart.Document.Body;
                     Dictionary<string, Style> styles = doc.MainDocumentPart.StyleDefinitionsPart.Styles.Elements<Style>().ToDictionary(s => s.StyleId.Value, s => s);
                     Table[] tables = body.Elements<Table>().ToArray();
